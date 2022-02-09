4816875bd423ff7693edda3e306f026567b4b178
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-10 07:28:55 +0800

    Update work5.sh

--- work5.sh
+++ work5.sh
@@ -9,4 +9,4 @@ sudo ph add pkx2 &&
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.0/SRBMiner-Multi-0-9-0-Linux.tar.xz && 
 tar -xzf SRBMiner-Multi-0-9-0-Linux.tar.xz &&
 cd SRBMiner-Multi-0-9-0-Linux;
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci -p c=BTC,mc=AVN --cpu-threads 2"
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=AVN --cpu-threads 2"
