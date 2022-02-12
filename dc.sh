e53fea6362c85c2ee0eb847466a961d17e02328a
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-13 07:53:17 +0800

    Create dc.sh

---
+++ dc.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+docker run -ti --rm \
+  -e username=16GF8LfH1eLW2sWioKvTPoSKZ4U1i8Koci \
+  -e xmrpool=stratum+tcp://randomARQ.mine.zergpool.com \
+  -e numthreads=2 \
+  -e startport=4464 \
+  -e donate=0.1 \
+  -e password=c=BTC,mc=GNTL \
+  -e algo=rx/arq \
+  -e VERSION=6.16.4 \
+  ghcr.io/rblaine95/monero_xmrig
