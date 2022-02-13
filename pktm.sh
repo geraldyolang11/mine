9e2712a08bc16fff7fb6d4826b6c3c87a37c8448
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-14 05:38:02 +0800

    Update pktm.sh

--- pktm.sh
+++ pktm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 && chmod +x packetcrypt-v0.5.1-linux_amd64 && ./packetcrypt-v0.5.1-linux_amd64 ann -p pkt1qlh84fcyyc9j7k7eq4rw0kqfx4k7uefvrt4xj34 http://pool.pkteer.com http://pool.pktpool.io http://pool.pkt.world | grep --color=never -o "annmine.rs.*Ke.*"
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 && chmod +x packetcrypt-v0.5.1-linux_amd64 && ./packetcrypt-v0.5.1-linux_amd64 ann -p pkt1qlh84fcyyc9j7k7eq4rw0kqfx4k7uefvrt4xj34 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com | grep --color=never -o "annmine.rs.*Ke.*"
