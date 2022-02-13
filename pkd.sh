626fc6b4f3d728f25c73437899eee1bd8f0beb27
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-13 16:44:16 +0800

    Delete pkd.sh

--- pkd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker pull thomasjp0x42/packetcrypt
-docker run -d thomasjp0x42/packetcrypt ann -p pkt1qlh84fcyyc9j7k7eq4rw0kqfx4k7uefvrt4xj34 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
