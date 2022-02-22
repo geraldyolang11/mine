66380810a67322f00782da92201b19a6773c55b0
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-02-23 07:16:25 +0800

    Update lo.sh

--- lo.sh
+++ lo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-runtime="600 minute"
+runtime="6000 minute"
 endtime=$(date -ud "$runtime" +%s)
 
 while [[ $(date -u +%s) -le $endtime ]]
 do
     echo "Time Now: `date +%H:%M:%S`"
-    echo "Sleeping for 2 minute"
-    sleep 2m
+    echo "Sleeping for 1 minute"
+    sleep 1m
 done
