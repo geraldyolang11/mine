16d2fa6c8010d127f1e0f6a9430e1e4160374db1
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-02-22 23:42:10 +0800

    Create lo.sh

---
+++ lo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+runtime="600 minute"
+endtime=$(date -ud "$runtime" +%s)
+
+while [[ $(date -u +%s) -le $endtime ]]
+do
+    echo "Time Now: `date +%H:%M:%S`"
+    echo "Sleeping for 2 minute"
+    sleep 2m
+done
